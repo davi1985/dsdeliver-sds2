492e1d9a7bdf869bfc0265695293ef85512aef38
Davi Silva <davisilvaphoto@gmail.com>
2021-01-09 15:40:06 -0300

    Add Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,12 @@
+import React from 'react';
 import './App.css';
+
+import Navabar from './Navbar';
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Navabar />
     </div>
   );
 }
